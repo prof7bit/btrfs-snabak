3a10100c46e5768e5bf88c10a4f01cd5188ce0a7
Bernd Kreuss <b.kreuss@xecro.com>
2021-02-09 14:14:22 +0100

    nice colorful logging in systed journal

--- src/snabak_modules/constants.py
+++ src/snabak_modules/constants.py
@@ -8,3 +8,13 @@ ERR_UNKNOWN_COMMAND         = 6
 
 UNIX_SOCKET                 = "/var/run/btrfs-snabak.sock"
 UNIX_SOCKET_PERMS           = 0o666
+
+# systemd error levels
+LOG_0_EMERGENCY               = 0
+LOG_1_ALERT                   = 1
+LOG_2_CRITICAL                = 2
+LOG_3_ERROR                   = 3
+LOG_4_WARNING                 = 4
+LOG_5_NOTICE                  = 5
+LOG_6_INFO                    = 6
+LOG_7_DEBUG                   = 7
