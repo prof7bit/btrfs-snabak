5055bd8ec3efc368db498193e0094a4705f3f098
Bernd K <prof7bit@gmail.com>
2021-02-09 22:39:29 +0100

    config: parse int, bool and list entries

--- src/snabak_modules/config.py
+++ src/snabak_modules/config.py
@@ -25,11 +25,33 @@ class Config:
             self.cp = cp
             self.sect = sect
 
+        class Item(str):
+            def __new__(cls, value):
+                return super().__new__(cls, value)
+
+            def as_int(self):
+                if self == '':
+                    return 0
+                else:
+                    return int(self)  # can raise ValueError
+
+            def as_bool(self):
+                s = self.lower()
+                if s in ['0', 'f', 'n', 'no', 'false', 'off']:
+                    return False
+                elif s in ['1', 't', 'y', 'yes', 'true', 'on']:
+                    return True
+                else:
+                    raise ValueError
+
+            def as_list(self):
+                return list(filter(None, self.replace(' ', ',').split(',')))
+
         def __getattr__(self, item):
             try:
-                return self.cp.get(self.sect, item)
+                return Config.Section.Item(self.cp.get(self.sect, item))
             except (NoOptionError, NoSectionError):
-                return ''
+                return Config.Section.Item('')
 
     def __getattr__(self, item):
         return Config.Section(self.cp, item)
