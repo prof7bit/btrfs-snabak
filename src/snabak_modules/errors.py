0cc778e2e8f2e091d76de4e7d1fe2c6d4289a3ef
Bernd Kreuss <b.kreuss@xecro.com>
2021-02-09 11:10:21 +0100

    use common module for error constants, refactor some things

---
+++ src/snabak_modules/errors.py
@@ -0,0 +1,7 @@
+ERR_OK                      = 0
+ERR_DAEMON                  = 1
+ERR_NOT_IMPLEMENTED         = 2
+ERR_USAGE                   = 3
+ERR_INVALID_CONFIG_NAME     = 4
+ERR_UNKNOWN_EXCEPTION       = 5
+ERR_UNKNOWN_COMMAND         = 6
