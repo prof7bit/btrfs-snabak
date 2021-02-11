5801133d28beb46c1047ae723cfeefdb5b2ded82
Bernd Kreuss <b.kreuss@xecro.com>
2021-02-11 13:23:38 +0100

    implement snapshot delete

--- src/snabak_modules/commands.py
+++ src/snabak_modules/commands.py
@@ -2,14 +2,15 @@ import json
 
 
 class Command:
-    def __init__(self, cmd, config):
+    def __init__(self, cmd, config, number=None):
         self.cmd = cmd
         self.config = config
+        self.number = number
 
     @classmethod
     def from_json(cls, js):
         o = json.loads(js)
-        return cls(o["cmd"], o["config"])
+        return cls(o["cmd"], o["config"], o["number"])
 
     def serialize(self):
         return json.dumps(self.__dict__).encode()
