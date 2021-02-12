77879877d7c8a0d2cf76fc8816a8076af44bdf53
Bernd Kreuss <b.kreuss@xecro.com>
2021-02-12 09:25:29 +0100

    test whether btrfs-snabak actually exists before executing any of the completion stuff

--- system-files/btrfs-snabak-completion.sh
+++ system-files/btrfs-snabak-completion.sh
@@ -1,17 +1,21 @@
 _btrfs-snabak()
 {
-    local cur prev prev2 words
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    prev2="${COMP_WORDS[COMP_CWORD-2]}"
+    COMPREPLY=()
+    if command -v btrfs-snabak &> /dev/null
+    then
+      local cur prev prev2 words
+      cur="${COMP_WORDS[COMP_CWORD]}"
+      prev="${COMP_WORDS[COMP_CWORD-1]}"
+      prev2="${COMP_WORDS[COMP_CWORD-2]}"
 
-    # The entire completion logic is implemented in
-    # the tool itself: Given the two previous words
-    # on the command line its subcommand "compassist"
-    # will dump the list of possible completions.
-    words=$(btrfs-snabak compassist "$prev" "$prev2")
+      # The entire completion logic is implemented in
+      # the tool itself: Given the two previous words
+      # on the command line its subcommand "compassist"
+      # will dump the list of possible completions.
+      words=$(btrfs-snabak compassist "$prev" "$prev2")
 
-    COMPREPLY=( $(compgen -W "$words" -- "$cur") )
+      COMPREPLY=( $(compgen -W "$words" -- "$cur") )
+    fi
     return 0
 }
 complete -F _btrfs-snabak btrfs-snabak
